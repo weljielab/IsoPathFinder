c50e301f48058116b8ef6127f5339711b49bb925
srhoades10 <srhoades10@gmail.com>
2018-06-28 11:37:30 -0500

    Dockerfile added as test run

--- SerineExample.sh
+++ SerineExample.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-python ReactionModeling.py
-python IsoPathFinderDemo.py
+
+#Build the labeled metabolic network - note this example builds 20 reaction rounds
+python3 ReactionModeling.py
+
+#Check that the network exists, then run a BuildPaths('Serine M+3', 9) example
+python3 IsoPathFinderDemo.py
